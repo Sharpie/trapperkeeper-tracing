664530172f105c42e0d7d0de2374c4a848158edd
Charlie Sharpsteen <source@sharpsteen.net>
2018-04-07 08:25:22 -0700

    Add support for setting parent spans
    This commit updates the `build-span` function to respond to a `:child-of`
    key in its option map. When supplied, this key is interpreted as a
    Span or SpanContext instance that is used as the parent of the newly built
    span.

--- test/unit/sharpie/trapperkeeper/tracing_test.clj
+++ test/unit/sharpie/trapperkeeper/tracing_test.clj
@@ -3,7 +3,7 @@
     [clojure.test :refer :all]
     [sharpie.trapperkeeper.tracing :as tracing])
   (:import
-    (io.opentracing.mock MockTracer)
+    (io.opentracing.mock MockSpan$MockContext MockTracer)
     (io.opentracing.noop NoopTracerFactory)))
 
 (def noop-tracer (NoopTracerFactory/create))
@@ -27,7 +27,20 @@
   (binding [tracing/*tracer* mock-tracer]
     (testing "Calling build-span with an operation name produces a span with that name."
       (let [span (tracing/build-span "test span")]
-        (is (= "test span" (.operationName span)))))))
+        (is (= "test span" (.operationName span)))))
+
+    (testing "Calling build-span with a parent context produces a span that is a child of that parent."
+      (let [parent-context (new MockSpan$MockContext
+                                (long 9000)
+                                (long 42)
+                                {"test-baggage-item" "foo"})
+            span (tracing/build-span "test span with parent"
+                                     {:child-of parent-context})]
+
+        (is (= 9000 (-> span .context .traceId)))
+        (is (= 42 (.parentId span)))
+        (is (= "foo" (.getBaggageItem span "test-baggage-item")))))))
+
 
 (deftest trace-test
   (binding [tracing/*tracer* mock-tracer]
@@ -48,4 +61,18 @@
         (let [span (-> tracing/*tracer* .finishedSpans first)
               _ (.reset tracing/*tracer*)]
           (is (>= (- (.finishMicros span) (.startMicros span))
-                (long 0.5e6)))))))
+                (long 0.5e6)))))
+
+    (testing "Span options can be set by passing a map to trace"
+      (tracing/trace {:id "test trace with options"
+                      :child-of (new MockSpan$MockContext
+                                     (long 8140)
+                                     (long 66)
+                                     {"bar-baggage-item" "bar"})}
+                     :noop)
+        (let [span (-> tracing/*tracer* .finishedSpans first)
+              _ (.reset tracing/*tracer*)]
+          (is (= "test trace with options" (.operationName span)))
+          (is (= 8140 (-> span .context .traceId)))
+          (is (= 66 (.parentId span)))
+          (is (= "bar" (.getBaggageItem span "bar-baggage-item")))))))
